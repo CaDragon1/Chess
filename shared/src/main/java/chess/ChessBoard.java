127c21f6752feda26ff2796b400004aeb62d9738
Caleb Drake <cadragon11@gmail.com>
2024-06-26 22:49:57 -0500

    Implemented game board representation as well as the logic to set the board up. Boards are simply (though perhaps not ideally) represented by a 2d array of ChessPiece objects.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,8 +8,10 @@ package chess;
  */
 public class ChessBoard {
 
+    ChessPiece[][] board;
+
     public ChessBoard() {
-        
+        board = new ChessPiece[8][8];
     }
 
     /**
@@ -19,7 +21,8 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getRow()][position.getColumn()] = piece; /* This does not check to see if the space is empty.
+                                                               Depending on how the tests work, may need to be fixed. */
     }
 
     /**
@@ -30,7 +33,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow()][position.getColumn()];
     }
 
     /**
@@ -38,6 +41,65 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        clearBoard();
+        setPawns(ChessGame.TeamColor.BLACK, 2);
+        setPawns(ChessGame.TeamColor.WHITE, 7);
+        setPawns(ChessGame.TeamColor.BLACK, 1);
+        setBackRow(ChessGame.TeamColor.WHITE, 8);
+
+    }
+
+    //This function clears the board entirely.
+    private void clearBoard(){
+        for(int row = 0; row < 8; row++){
+            for(int column = 0; column < 8; column++){
+                board[row][column] = null;
+            }
+        }
+    }
+
+    // This function sets a row of pawns of a given color to a given row index.
+    private void setPawns(ChessGame.TeamColor teamColor, int row){
+        for (int column = 0; column < 8; column++) {
+            board[row][column] = new ChessPiece(teamColor, ChessPiece.PieceType.PAWN);
+        }
+    }
+
+    // This function sets up the back starting row of a normal chess game at a given row index.
+    private void setBackRow(ChessGame.TeamColor teamColor, int row){
+        for (int column = 0; column < 8; column++) {
+
+            /*if(column == 0 || column == 7){
+                board[row][column] = new ChessPiece(teamColor, ChessPiece.PieceType.ROOK);
+            }
+            else if(column == 1 || column == 6){
+                board[row][column] = new ChessPiece(teamColor, ChessPiece.PieceType.KNIGHT);
+            }
+            else if(column == 2 || column == 5){
+                board[row][column] = new ChessPiece(teamColor, ChessPiece.PieceType.BISHOP);
+            }
+            else if(column == 3){
+                board[row][column] = new ChessPiece(teamColor, ChessPiece.PieceType.QUEEN);
+            }
+            else{
+                board[row][column] = new ChessPiece(teamColor, ChessPiece.PieceType.KING);*/
+
+            switch (column){
+                case 0:
+                case 7:
+                    board[row][column] = new ChessPiece(teamColor, ChessPiece.PieceType.ROOK);
+                case 1:
+                case 6:
+                    board[row][column] = new ChessPiece(teamColor, ChessPiece.PieceType.KNIGHT);
+                case 2:
+                case 5:
+                    board[row][column] = new ChessPiece(teamColor, ChessPiece.PieceType.BISHOP);
+                case 3:
+                    board[row][column] = new ChessPiece(teamColor, ChessPiece.PieceType.QUEEN);
+                case 4:
+                    board[row][column] = new ChessPiece(teamColor, ChessPiece.PieceType.KING);
+            }
+
+        }
     }
 }
