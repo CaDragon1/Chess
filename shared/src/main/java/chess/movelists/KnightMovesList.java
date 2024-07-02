4159889d204980847505e3bf6d59261185c718c0
Caleb Drake <cadragon11@gmail.com>
2024-07-01 23:57:18 -0500

    Fixed an error in the knight's code that prevented the loop from running through

--- shared/src/main/java/chess/movelists/KnightMovesList.java
+++ shared/src/main/java/chess/movelists/KnightMovesList.java
@@ -28,13 +28,17 @@ public class KnightMovesList extends MovesList {
     @Override
     void calculateMove(ChessPosition myPosition) {
         for (int column = myPosition.getColumn() - 2; column <= myPosition.getColumn() + 2; column++) {
-            for (int row = myPosition.getRow() + 2; row <= myPosition.getRow() - 2; row--) {
+            System.out.println("Column: " + column);
+            for (int row = myPosition.getRow() + 2; row >= myPosition.getRow() - 2; row--) {
+                System.out.println("Row: " + row);
                 if(Math.abs(myPosition.getColumn() - column) + Math.abs(myPosition.getRow() - row) == 3
                 && row > 0 && row <= 8 && column > 0 && column <= 8) {
+                    System.out.println("Checking position " + row + " , " + column);
                     ChessPosition checkingPosition = new ChessPosition(row, column);
                     // Make sure positions aren't taken up by friendly pieces
                     if(board.getPiece(checkingPosition) == null || board.getPiece(checkingPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()){
-                        pieceMoves.add(new ChessMove(myPosition, checkingPosition, null));
+                        System.out.println("Adding!");
+                        pieceMoves.add(new ChessMove(myPosition, new ChessPosition(checkingPosition.getRow(), checkingPosition.getColumn()), null));
                     }
                 }
             }
