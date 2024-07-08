a5b30b93577897f9d4d0a4b6ba44eb4b291085ad
Caleb Drake <cadragon11@gmail.com>
2024-07-07 19:06:15 -0500

    Implemented some base functions, added some necessary functionsfor future logic implementation

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,5 +1,6 @@
 package chess;
 
+import javax.swing.text.Position;
 import java.util.Collection;
 
 /**
@@ -9,16 +10,22 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessGame {
+    private TeamColor teamTurn;
+    private ChessBoard board;
+    private ChessBoard checkingBoard;
 
     public ChessGame() {
-
+        teamTurn = TeamColor.WHITE;
+        board = new ChessBoard();
+        checkingBoard = new ChessBoard();
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+
+        return teamTurn;
     }
 
     /**
@@ -27,7 +34,8 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+
+        teamTurn = team;
     }
 
     /**
@@ -46,7 +54,8 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        throw new RuntimeException("Not implemented");
+
+        return board.getPiece(startPosition).pieceMoves(board, startPosition);
     }
 
     /**
@@ -56,7 +65,11 @@ public class ChessGame {
      * @throws InvalidMoveException if move is invalid
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
-        throw new RuntimeException("Not implemented");
+        checkingBoard = board;
+        if (validMoves(move.getStartPosition()).contains(move)){
+            checkingBoard.addPiece(move.getEndPosition(), board.getPiece(move.getStartPosition()));
+
+        }
     }
 
     /**
@@ -88,6 +101,42 @@ public class ChessGame {
      */
     public boolean isInStalemate(TeamColor teamColor) {
         throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Given a board, determines if a team is in checkmate on that board.
+     * This is useful for checking if a move will put your king in check, and it can also be called by isInCheck.
+     * @param gameBoard is the board used to determine if said team is in check (a temporary board is given when
+     *                    determining if a move will put the king in check).
+     * @param teamColor is the team who we are determining check status for.
+     * @return false if the team is not in check, true if it is.
+     */
+    public boolean putsInCheck(ChessBoard gameBoard, TeamColor teamColor) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * findKing locates what position the king of the given teamColor is in.
+     * @param gameBoard is the board we are looking at (typically the regular board).
+     * @param teamColor is the team we are finding the king of.
+     * @return the position of the king. If no king is found, returns null.
+     */
+    public ChessPosition findKing(ChessBoard gameBoard, TeamColor teamColor) {
+        ChessPosition currentPosition = new ChessPosition();
+
+        for (int row = 1; row <= 8; row++){
+            currentPosition.setRowValue(row);
+            for (int col = 1; col <= 8; col++){
+                currentPosition.setColValue(col);
+                if (gameBoard.getPiece(currentPosition) != null){
+                    if (gameBoard.getPiece(currentPosition).getTeamColor() == teamColor
+                    && gameBoard.getPiece(currentPosition).getPieceType() == ChessPiece.PieceType.KING){
+                        return currentPosition;
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     /**
